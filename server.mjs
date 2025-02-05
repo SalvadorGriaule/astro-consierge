01f16233535ca98e9af82493ecfd3ec610b73c2d
Morales quentin <morales.quentin@gmx.fr>
2025-02-05 14:09:44 +0100

    bcrypt : OK

--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
 import express from "express"
 import bodyParser from "body-parser"
+import bcrypt from "bcrypt"
 import { handler as astroHandler } from "./dist/server/entry.mjs";
 import { initTable, User } from "./src/db/db.js";
 
+// express const
 const app = express();
 const PORT = 3000;
 const base = '/'
+// bcrypt const
+const saltRound = 10;
 
 app.use(base, express.static("dist/client/"));
 app.use(astroHandler);
@@ -14,10 +18,18 @@ app.use(bodyParser.urlencoded({extended: false}))
 initTable()
 
 app.post("/signIn/post", async (req, res) => {
-    console.dir(req.body);
     const data = req.body;
-    const insert = await User.create(data)
-    console.log(`Insertion of ${insert.id} : OK`)
+    bcrypt.genSalt(saltRound, (err,salt) => {
+        bcrypt.hash(data.password, salt, async (err,hash) => {
+            data.password = hash
+            try {
+                const insert = await User.create(data)
+                res.redirect("/")
+            } catch(e){
+                console.log(e)
+            }
+        })
+    })
 })
 
 
