4ace37fba02fa7425e69d4d1db343c16d247f90f
Morales quentin <morales.quentin@gmx.fr>
2025-02-05 18:55:45 +0100

    session setup

--- server.mjs
+++ server.mjs
@@ -1,8 +1,13 @@
 import express from "express"
 import bodyParser from "body-parser"
 import bcrypt from "bcrypt"
+import dotenv from "dotenv"
+import jwt from "jsonwebtoken"
+import cookiesParser from "cookie-parser"
+import session from "express-session"
 import { handler as astroHandler } from "./dist/server/entry.mjs";
 import { initTable, User } from "./src/db/db.js";
+import * as crypto from "crypto"
 
 // express const
 const app = express();
@@ -10,22 +15,37 @@ const PORT = 3000;
 const base = '/'
 // bcrypt const
 const saltRound = 10;
+// key const
+dotenv.config();
+const sessionEnv = process.env.SESSION_KEY
+
+const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: { type: "spki", format: "pem"},
+    privateKeyEncoding: { type: "pkcs8", format: "pem"}
+})
 
 app.use(base, express.static("dist/client/"));
 app.use(astroHandler);
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookiesParser());
+app.use(session({
+    secret: sessionEnv,
+    resave: false,
+    saveUninitialized: true
+}))
 
 initTable()
 
 app.post("/signIn/post", async (req, res) => {
     const data = req.body;
-    bcrypt.genSalt(saltRound, (err,salt) => {
-        bcrypt.hash(data.password, salt, async (err,hash) => {
+    bcrypt.genSalt(saltRound, (err, salt) => {
+        bcrypt.hash(data.password, salt, async (err, hash) => {
             data.password = hash
             try {
                 const insert = await User.create(data)
                 res.redirect("/")
-            } catch(e){
+            } catch (e) {
                 console.log(e)
                 res.send("acces denid")
             }
@@ -35,9 +55,17 @@ app.post("/signIn/post", async (req, res) => {
 
 app.post("/login/post", async (req, res) => {
     const data = req.body;
-    const auth = await User.findOne({ where: { email:data.email }});
-    bcrypt.compare(data.password, auth.password, (err,result) => {
-        result ? res.redirect(`/UserSpace/${auth.id}`) : res.send("Acces denided")
+    const auth = await User.findOne({ where: { email: data.email } });
+    bcrypt.compare(data.password, auth.password, (err, result) => {
+        if (result) {
+            jwt.sign({ user:auth.username }, privateKey,{ algorithm: 'RS256'}, (err, token) => {
+                req.session.role = "user"
+                req.session.jwt = token  
+            })
+            res.redirect(`/UserSpace/${auth.id}`)
+        } else {
+            res.send("Acces denided")
+        }
     })
 })
 
